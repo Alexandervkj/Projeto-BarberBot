f22db7319a46a8e37e58ace2a84f6d492ce63088
Alexandervkj <alexandeer_junior@hotmail.com>
2024-09-21 10:45:22 -0300

    Modelagem do Banco de dados, definindo os  modelos para Barbearias e agendamentos no arquivo models.py do app Scheduling

--- scheduling/models.py
+++ scheduling/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+
+class BarberShop(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    phone = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.name
+
+class Appointment(models.Model):
+    client_name = models.CharField(max_length=100)
+    date = models.DateField()
+    time = models.TimeField()
+    barber_shop = models.ForeignKey(BarberShop, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.client_name} - {self.date} {self.time}'
