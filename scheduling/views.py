af3db6d4a7d61e9515cda8eea9cac94ff7ed3f95
Alexandervkj <alexandeer_junior@hotmail.com>
2024-09-21 10:48:45 -0300

    Criação de Views e Templates: View para Listar Barbearias e Criar Agendamentos, no arquivo views.py do app scheduling.

--- scheduling/views.py
+++ scheduling/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from .models import BarberShop, Appointment
 
-# Create your views here.
+def appointment_view(request):
+    barber_shops = BarberShop.objects.all()
+
+    if request.method == 'POST':
+        client_name = request.POST['client_name']
+        date = request.POST['date']
+        time = request.POST['time']
+        barber_shop = BarberShop.objects.get(id=request.POST['barber_shop'])
+
+        # Criar agendamento
+        appointment = Appointment.objects.create(
+            client_name=client_name,
+            date=date,
+            time=time,
+            barber_shop=barber_shop
+        )
+
+        return render(request, 'success.html', {'appointment': appointment})
+
+    return render(request, 'appointment_form.html', {'barber_shops': barber_shops})
